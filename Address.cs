61d0248b558488b8783499975e95cda488481261
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-12 20:14:37 +0530

    Adding Multiple Contact And Display

--- Address.cs
+++ Address.cs
@@ -10,9 +10,26 @@ namespace AddressBookManagement
     {
         List<Contact> contacts = new List<Contact>();
         Contact contact;
+        Dictionary<string, List<Contact>> addressbookSystem = new Dictionary<string, List<Contact>>();
         public void Entry()
         {
             contact = new Contact();
+
+            bool status = true;
+            while (status)
+            {
+                Console.WriteLine("Enter your first_name");
+                contact.first_Name = Console.ReadLine();
+                if (contacts.Any(data => (data.first_Name == contact.first_Name)))
+                {
+                    Console.WriteLine("Contact Name already exists");
+                }
+                else
+                {
+                    status = false;
+                }
+            }
+
             Console.WriteLine("Enter your first_name");
             contact.First_Name = Console.ReadLine();
             Console.WriteLine("Enter your last_name");
@@ -37,88 +54,126 @@ namespace AddressBookManagement
                 Console.WriteLine(Contact.ToString());
             }
         }
-
-        public void Edit()
+        //uc5
+        public void Display()
         {
-            Console.WriteLine("Enter the name of contact do you want to edit : ");
-            string name = Convert.ToString(Console.ReadLine());
-            foreach (var data in contacts)
+            foreach (var keyValuePair in addressbookSystem.Keys)
             {
-                if (data.first_Name == name)
+                Console.WriteLine("Book Name:" + keyValuePair);
+                foreach (Contact data in addressbookSystem[keyValuePair])
                 {
-                    Console.WriteLine("choose the option to change the data \n1)firstName\n2)lastName\n3)Present_Address\n4)City\n5)State\n6)Zip\n7)PhoneNumber\n8)Email_Id");
-                    int choice = Convert.ToInt32(Console.ReadLine());
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.WriteLine("Please enter the first name : ");
-                            string firstName = Convert.ToString(Console.ReadLine());
-                            data.first_Name = firstName;
-                            break;
-                        case 2:
-                            Console.WriteLine("Please enter the last name : ");
-                            string lastName = Convert.ToString(Console.ReadLine());
-                            data.last_Name = lastName;
-                            break;
-                        case 3:
-                            Console.WriteLine("Please enter the Address : ");
-                            string address = Convert.ToString(Console.ReadLine());
-                            data.present_Address = address;
-                            break;
-                        case 4:
-                            Console.WriteLine("Please enter the city : ");
-                            string City = Convert.ToString(Console.ReadLine());
-                            data.city = City;
-                            break;
-                        case 5:
-                            Console.WriteLine("Please enter the city : ");
-                            string State = Convert.ToString(Console.ReadLine());
-                            data.state = State;
-                            break;
-                        case 6:
-                            Console.WriteLine("Please enter the zip Code : ");
-                            string ZipCode = Convert.ToString(Console.ReadLine());
-                            data.zip = ZipCode;
-                            break;
-                        case 7:
-                            Console.WriteLine("Please enter the Phone Number : ");
-                            string PhoneNumber = Convert.ToString(Console.ReadLine());
-                            data.phone_Number = PhoneNumber;
-                            break;
-                        case 8:
+                    Console.WriteLine($"***** Contact Details in Book :{keyValuePair} Display *****");
+                    Console.WriteLine(data.first_Name);
+                    Console.WriteLine(data.last_Name);
+                    Console.WriteLine(data.present_Address);
+                    Console.WriteLine(data.city);
+                    Console.WriteLine(data.state);
+                    Console.WriteLine(data.zip);
+                    Console.WriteLine(data.phone_Number);
+                    Console.WriteLine(data.email_Id);
 
-                            Console.WriteLine("Please enter the email : ");
-                            string Email = Convert.ToString(Console.ReadLine());
-                            data.email_Id = Email;
-                            break;
-                        default:
-                            Console.WriteLine("please choose from above options :");
-                            break;
-                    }
-                    Console.WriteLine("\nAfter Editing Contact Details:");
-                    Console.WriteLine(contact.ToString());
-                    Console.WriteLine();
                 }
-                else
-                {
-                    //            Console.WriteLine("Contact not found{0}", name);
-                }
-            }
-
-            public void RemoveDetails()
-            {
-                Console.WriteLine("Enter the first Name");
-                string FirstName = Console.ReadLine();
-                Console.WriteLine("Enter the last Name");
-                string LastName = Console.ReadLine();
-                Contact contact = new Contact();
-                contact.Remove(contact);
-                Console.WriteLine("Contacts are successfully Deleted");
-                Console.ReadLine();
-
-
-
             }
         }
+
     }
+
+    //uc3
+    public void Edit()
+    {
+        Console.WriteLine("Enter the name of contact do you want to edit : ");
+        string name = Convert.ToString(Console.ReadLine());
+        foreach (var data in contacts)
+        {
+            if (data.first_Name == name)
+            {
+                Console.WriteLine("choose the option to change the data \n1)firstName\n2)lastName\n3)Present_Address\n4)City\n5)State\n6)Zip\n7)PhoneNumber\n8)Email_Id");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Please enter the first name : ");
+                        string firstName = Convert.ToString(Console.ReadLine());
+                        data.first_Name = firstName;
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter the last name : ");
+                        string lastName = Convert.ToString(Console.ReadLine());
+                        data.last_Name = lastName;
+                        break;
+                    case 3:
+                        Console.WriteLine("Please enter the Address : ");
+                        string address = Convert.ToString(Console.ReadLine());
+                        data.present_Address = address;
+                        break;
+                    case 4:
+                        Console.WriteLine("Please enter the city : ");
+                        string City = Convert.ToString(Console.ReadLine());
+                        data.city = City;
+                        break;
+                    case 5:
+                        Console.WriteLine("Please enter the city : ");
+                        string State = Convert.ToString(Console.ReadLine());
+                        data.state = State;
+                        break;
+                    case 6:
+                        Console.WriteLine("Please enter the zip Code : ");
+                        string ZipCode = Convert.ToString(Console.ReadLine());
+                        data.zip = ZipCode;
+                        break;
+                    case 7:
+                        Console.WriteLine("Please enter the Phone Number : ");
+                        string PhoneNumber = Convert.ToString(Console.ReadLine());
+                        data.phone_Number = PhoneNumber;
+                        break;
+                    case 8:
+
+                        Console.WriteLine("Please enter the email : ");
+                        string Email = Convert.ToString(Console.ReadLine());
+                        data.email_Id = Email;
+                        break;
+                    default:
+                        Console.WriteLine("please choose from above options :");
+                        break;
+                }
+                Console.WriteLine("\nAfter Editing Contact Details:");
+                Console.WriteLine(contact.ToString());
+                Console.WriteLine();
+            }
+            else
+            {
+                //            Console.WriteLine("Contact not found{0}", name);
+            }
+        }
+        //uc4
+        public void RemoveDetails()
+        {
+            Console.WriteLine("Enter the first Name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter the last Name");
+            string LastName = Console.ReadLine();
+            Contact contact = new Contact();
+            contact.Remove(contact);
+            Console.WriteLine("Contacts are successfully Deleted");
+            Console.ReadLine();
+
+
+
+        }
+        //uc6
+        public void AddMultipleContact()
+        {
+            Console.WriteLine("Enter Number of Contacts to be Added:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            while (input > 0)
+            {
+                Entry();
+                input--;
+            }
+    addressbookSystem.Add(bookname, contacts);
+
+
+
+}
+}
 }
